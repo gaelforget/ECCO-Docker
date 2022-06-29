8c4c953dbd123cd07306ef263a8191b510797bde
gaelforget <gforget@mit.edu>
2022-06-29 08:32:57 +0200

    try with quiet option (no space left on device)

--- src/build_MITgcm_ECCO.sh
+++ src/build_MITgcm_ECCO.sh
@@ -9,8 +9,8 @@ mv ECCOv4 MITgcm/mysetups/.
 #export MPI_INC_DIR=/usr/lib/x86_64-linux-gnu/openmpi/include
 
 cd MITgcm/mysetups/ECCOv4/build
-../../../tools/genmake2 -mods=../code -mpi
-make depend
-make -j 4
+../../../tools/genmake2 -mods=../code -mpi > tmp.genmake.txt
+make depend > tmp.makedepend.txt
+make -j 4 > tmp.make.txt
 cd ..
 
