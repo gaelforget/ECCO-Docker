3878dababc7e27008843e803ca599b037b19a195
gaelforget <gforget@mit.edu>
2024-06-19 21:51:56 -0400

    updates, avoid downloads

--- src/warmup.jl
+++ src/warmup.jl
@@ -1,6 +1,6 @@
 using Pluto, CairoMakie, Downloads, IJulia, Pkg
 
-import MITgcm, OceanStateEstimation
+import MITgcm, Climatology
 import MITgcm.ClimateModels
 import MITgcm.MeshArrays
 using MITgcm.ClimateModels.Git
@@ -10,6 +10,7 @@ using MITgcm.ClimateModels.Git
 p0=pathof(MITgcm)
 fil=joinpath(dirname(p0),"..","examples","configurations","OCCA2.toml")
 MC=MITgcm.MITgcm_config(inputs=MITgcm.read_toml(fil))
+push!(MC.inputs[:setup][:main],(:input_folder => tempname()))
 ClimateModels.setup(MC)
 ClimateModels.build(MC)
 
@@ -29,18 +30,18 @@ ClimateModels.launch(tmp)
 ##
 
 MeshArrays.GRID_LLC90_download()
-OceanStateEstimation.ECCOdiags_add("release2")
-OceanStateEstimation.ECCOdiags_add("release4")
+Climatology.ECCOdiags_add("release2")
+Climatology.ECCOdiags_add("release4")
 
 Downloads.download(
   "https://zenodo.org/record/5784905/files/interp_coeffs_halfdeg.jld2",
-  joinpath(OceanStateEstimation.ScratchSpaces.ECCO,"interp_coeffs_halfdeg.jld2");
+  joinpath(Climatology.ScratchSpaces.ECCO,"interp_coeffs_halfdeg.jld2");
   timeout=60000.0)
 
 ##
 
-pth=joinpath(ENV["HOME"],"src","OceanStateEstimation.jl")
-run(`$(git()) clone https://github.com/gaelforget/OceanStateEstimation.jl $pth`)
+pth=joinpath(ENV["HOME"],"src","Climatology.jl")
+run(`$(git()) clone https://github.com/gaelforget/Climatology.jl $pth`)
 nb=joinpath(pth,"examples/ECCO/ECCO_standard_plots.jl")
 Pluto.activate_notebook_environment(nb)
 Pkg.instantiate()
